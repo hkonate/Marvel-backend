31e09fdb417e30cd9268bab1d32217aa77b0c287
hkonate <konate.tronca@gmail.com>
2022-12-11 09:06:14 +0100

    heroku error

--- routes/users.js
+++ routes/users.js
@@ -1,75 +1,73 @@
-const express = require("express")
-const uid2 = require("uid2")
-const SHA256 = require("crypto-js/sha256")
-const encBase64 = require("crypto-js/enc-base64")
+const express = require("express");
+const uid2 = require("uid2");
+const SHA256 = require("crypto-js/sha256");
+const encBase64 = require("crypto-js/enc-base64");
 const router = express.Router();
 
 const User = require("../models/User");
-const { builtinModules } = require("module");
 
 router.post("/user/signup", async (req, res) => {
-    try {
-        const { email, username, password } = req.body
+  try {
+    const { email, username, password } = req.body;
 
-        if (email && username && password) {
-            const doesEmailMatch = await User.findOne({ email: email })
-            if (!doesEmailMatch) {
-                const salt = uid2(16)
-                const hash = SHA256(password + salt).toString(encBase64)
-                const token = uid2(16)
-                const newUser = new User({
-                    email: email,
-                    username: username,
-                    token: token,
-                    salt: salt,
-                    hash: hash,
-                })
-                await newUser.save()
-                res.status(200).json({
-                    _id: newUser._id,
-                    username: newUser.username,
-                    email: newUser.email,
-                    token: newUser.token,
-                })
-            } else {
-                console.log("ok");
-                res.status(409).json({ message: "email does already exist" })
-            }
-        } else {
-            res.status(406).json({ message: "Missing information" })
-        }
-    } catch (error) {
-        res.status(400).json({ error: error.message })
+    if (email && username && password) {
+      const doesEmailMatch = await User.findOne({ email: email });
+      if (!doesEmailMatch) {
+        const salt = uid2(16);
+        const hash = SHA256(password + salt).toString(encBase64);
+        const token = uid2(16);
+        const newUser = new User({
+          email: email,
+          username: username,
+          token: token,
+          salt: salt,
+          hash: hash,
+        });
+        await newUser.save();
+        res.status(200).json({
+          _id: newUser._id,
+          username: newUser.username,
+          email: newUser.email,
+          token: newUser.token,
+        });
+      } else {
+        console.log("ok");
+        res.status(409).json({ message: "email does already exist" });
+      }
+    } else {
+      res.status(406).json({ message: "Missing information" });
     }
-})
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
 
 router.post("/user/login", async (req, res) => {
-    try {
-        const { password, email } = req.body
-        if (password && email) {
-            const user = await User.findOne({ email: email })
-            if (user) {
-                const hash = SHA256(password + user.salt).toString(encBase64);
-                if (hash === user.hash) {
-                    res.status(200).json({
-                        _id: user._id,
-                        username: user.username,
-                        email: user.email,
-                        token: user.token,
-                    })
-                } else {
-                    res.status(401).json({ error: "Unauthorized" })
-                }
-            } else {
-                res.status(400).json({ message: "User not found" })
-            }
+  try {
+    const { password, email } = req.body;
+    if (password && email) {
+      const user = await User.findOne({ email: email });
+      if (user) {
+        const hash = SHA256(password + user.salt).toString(encBase64);
+        if (hash === user.hash) {
+          res.status(200).json({
+            _id: user._id,
+            username: user.username,
+            email: user.email,
+            token: user.token,
+          });
         } else {
-            res.status(406).json({ message: "Missing informations" })
+          res.status(401).json({ error: "Unauthorized" });
         }
-    } catch (error) {
-        res.status(400).json({ error: error.message })
+      } else {
+        res.status(400).json({ message: "User not found" });
+      }
+    } else {
+      res.status(406).json({ message: "Missing informations" });
     }
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
 
-})
-
-module.exports = router
+module.exports = router;
--- server.js
+++ server.js
@@ -1,82 +1,73 @@
-require('dotenv').config();
-const axios = require('axios');
-const express = require('express')
-const cors = require('cors');
-const app = express()
-const cloudinary = require("cloudinary").v2;
-app.use(cors())
-app.use(express.json())
-const mongoose = require("mongoose")
+require("dotenv").config();
+const axios = require("axios");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+app.use(cors());
+app.use(express.json());
+const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI)
-cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.API_KEY_DB,
-    api_secret: process.env.API_SECRET,
-})
+mongoose.connect(process.env.MONGODB_URI);
 
+const favorisRoutes = require("./routes/favoris");
+app.use(favorisRoutes);
 
-
-
-const favorisRoutes = require("./routes/favoris")
-app.use(favorisRoutes)
-
-const usersRoutes = require("./routes/users")
-app.use(usersRoutes)
+const usersRoutes = require("./routes/users");
+app.use(usersRoutes);
 
 app.get("/", (req, res) => {
-    try {
-        console.log('dodo');
-        res.status(200).json({ message: "Welcome to the Marvel api !" })
-    } catch (error) {
-        console.log(error.message);
-    }
-})
+  try {
+    console.log("dodo");
+    res.status(200).json({ message: "Welcome to the Marvel api !" });
+  } catch (error) {
+    console.log(error.message);
+  }
+});
 
 app.get("/comics", async (req, res) => {
-    try {
-        const response = await axios.get(`https://app-marvel-react.herokuapp.com/comics?apiKey=${process.env.API_KEY}&skip=${req.query.skip}`);
-        res.status(200).json(response.data);
-    } catch (error) {
-        console.log(error.message);
-    }
-})
-
+  try {
+    const response = await axios.get(
+      `https://app-marvel-react.herokuapp.com/comics?apiKey=${process.env.API_KEY}&skip=${req.query.skip}`
+    );
+    res.status(200).json(response.data);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
 
 app.get("/comics/:characterId", async (req, res) => {
-    try {
-        const response = await axios.get(`https://app-marvel-react.herokuapp.com/comics/${req.params.characterId}apiKey=${process.env.API_KEY}`);
-        res.status(200).json(response.data);
-    } catch (error) {
-        console.log(error.message);
-    }
-})
+  try {
+    const response = await axios.get(
+      `https://app-marvel-react.herokuapp.com/comics/${req.params.characterId}apiKey=${process.env.API_KEY}`
+    );
+    res.status(200).json(response.data);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
 
 app.get("/characters", async (req, res) => {
-    try {
-        const response = await axios.get(`https://app-marvel-react.herokuapp.com/characters?apiKey=${process.env.API_KEY}&skip=${req.query.skip}`);
+  try {
+    const response = await axios.get(
+      `https://app-marvel-react.herokuapp.com/characters?apiKey=${process.env.API_KEY}&skip=${req.query.skip}`
+    );
 
-        res.status(200).json(response.data)
-    } catch (error) {
-        console.log(error.message);
-    }
-})
+    res.status(200).json(response.data);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
 
 app.get("/character/:characterId", async (req, res) => {
-    try {
-
-    } catch (error) {
-
-    }
-})
+  try {
+  } catch (error) {}
+});
 
 app.get("*", (req, res) => {
-    console.log("route not found");
-    res.status(404).json({ message: "This route doesn't exist" })
-})
+  console.log("route not found");
+  res.status(404).json({ message: "This route doesn't exist" });
+});
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log("Server live!!!!");
-})
-
-
+  console.log("Server live!!!!");
+});
